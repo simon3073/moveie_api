9bd225234d2c2f1bcd7709d66a79c437bddba2d1
simon3073 <simonbertoli@gmail.com>
2022-05-02 16:03:44 +1000

    Submission for Exerceise_2.5

--- index.js
+++ index.js
@@ -2,21 +2,107 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 
+// Temporary Data for Bonus Task
+let movies = [
+	{
+		Title: 'Planes, Trains & Automobiles',
+		Released: '1987',
+		Director: 'John Hughes',
+		Description: 'A Chicago advertising man must struggle to travel home from New York for Thanksgiving, with a lovable oaf of a shower curtain ring salesman as his only companion.',
+		imdbRating: 7.6,
+		imgURL: 'https://m.media-amazon.com/images/M/MV5BMjAwMDA2ODAwMV5BMl5BanBnXkFtZTYwMjkyNzE5._V1_.jpg',
+		Genre: ['Comedy', 'Drama'],
+		Actor: ['John Candy', 'Steve Martin']
+	},
+	{
+		Title: 'Commando',
+		Released: '1985',
+		Director: 'Mark L Lester',
+		Description: 'A retired Special Forces colonel tries to save his daughter, who was abducted by his former subordinate.',
+		imdbRating: 6.7,
+		imgURL: 'https://m.media-amazon.com/images/M/MV5BZWE0ZjFhYjItMzI5MC00MDllLWE4OGMtMzlhNGQzN2RjN2MwXkEyXkFqcGdeQXVyNDc2NjEyMw@@._V1_.jpg',
+		Genre: ['Action', 'Adventure', 'Thriller'],
+		Actor: ['Arnold Schwarzenegger', 'Dan Hedaya']
+	},
+	{
+		Title: 'Rambo',
+		Released: '1982',
+		Director: 'Ted Kotcheff',
+		Description: 'A veteran Green Beret is forced by a cruel Sheriff and his deputies to flee into the mountains and wage an escalating one-man war against his pursuers.',
+		imdbRating: 7.7,
+		imgURL: 'https://m.media-amazon.com/images/M/MV5BODBmOWU2YWMtZGUzZi00YzRhLWJjNDAtYTUwNWVkNDcyZmU5XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_.jpg',
+		Genre: ['Action', 'Adventure', 'Thriller'],
+		Actor: ['Sylvester Stallone', 'Richard Crenna']
+	}
+];
+
 // GET REQUESTS
 
 // Morgan logging to Terminal
 app.use(morgan('common'));
 
-// Default Response on / request
-app.get('/', (req, res) => {
-	res.send('80s Movie App');
+// Return a json list of all the movies
+app.get('/movies', (req, res) => {
+	res.json(movies);
 });
 
-// Return JSON Object of movies
-app.get('/movies', (req, res) => {
-	res.json({
-		'Fave 80s Movies': ['Bull Durham', 'The Breakfast Club', 'Sixteen Candles', 'Planes, Trains & Automobiles', 'Weird Science', "National Lampoon's Vacation", 'Pretty in Pink', 'Commando', 'Twins', 'Karate Kid']
-	});
+// Return a specific movie's details list of all the movies
+app.get('/movies/info/:movie', (req, res) => {
+	res.json(
+		movies.find((movie) => {
+			return movie.Title === req.params.movie;
+		})
+	);
+});
+
+// Return a directors bio
+app.get('/bio/:director', (req, res) => {
+	res.send('Return a directors bio');
+});
+
+// Return movies of a genre
+app.get('/movies/genre/:genre', (req, res) => {
+	res.send('Return movies of a genre');
+});
+
+// Return movies with an actor
+app.get('/movies/actor/:name', (req, res) => {
+	res.send('Return movies with an actor');
+});
+
+// Return movies with a minimum rating no
+app.get('/movies/rating/:rating', (req, res) => {
+	res.send('Return movies with a minimum rating number');
+});
+
+// Create an account
+app.post('/account', (req, res) => {
+	res.send('Create an account');
+});
+
+// get account details
+app.get('/account/:id', (req, res) => {
+	res.send('Get account details');
+});
+
+// Update Account details
+app.put('/account/:id', (req, res) => {
+	res.send('Update Account details');
+});
+
+// Deregister an account
+app.delete('/account/:id', (req, res) => {
+	res.send('Deregister an Account');
+});
+
+// Add movie to list
+app.put('/account/movies/:movie', (req, res) => {
+	res.send('Add movie to account list');
+});
+
+// Remove movie form account list
+app.delete('/account/movies/:movie', (req, res) => {
+	res.send('Remove movie form account list');
 });
 
 // Serve files from Public Folder
