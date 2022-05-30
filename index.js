b789b3559a06aaf43b2cc8436a24fb633d472561
simon3073 <simonbertoli@gmail.com>
2022-05-30 15:20:10 +1000

    Added Github Repo to Allowed CORS

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const auth = require('./auth')(app); // (app) added to pass Express to auth.js
 const passport = require('passport');
 
 // Import Authorisation & cors
-const allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234'];
+const allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'https://simon3073.github.io/movie_api_client/'];
 app.use(
 	cors({
 		origin: (origin, callback) => {
