254ccff99501461244e3629f46bbe6780b536aac
simon3073 <simonbertoli@gmail.com>
2022-06-07 16:48:52 +1000

    Change CORS back to not oepn

--- index.js
+++ index.js
@@ -23,21 +23,21 @@ const Genre = Models.Genre;
 mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Import Authorisation & cors
-// const allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'https://simon3073.github.io/'];
-// app.use(
-// 	cors({
-// 		origin: (origin, callback) => {
-// 			if (!origin) return callback(null, true);
-// 			if (allowedOrigins.indexOf(origin) === -1) {
-// 				// If a specific origin isn’t found on the list of allowed origins
-// 				let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-// 				return callback(new Error(message), false);
-// 			}
-// 			return callback(null, true);
-// 		}
-// 	})
-// );
-app.use(cors());
+const allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'https://simon3073.github.io/'];
+app.use(
+	cors({
+		origin: (origin, callback) => {
+			if (!origin) return callback(null, true);
+			if (allowedOrigins.indexOf(origin) === -1) {
+				// If a specific origin isn’t found on the list of allowed origins
+				let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+				return callback(new Error(message), false);
+			}
+			return callback(null, true);
+		}
+	})
+);
+// app.use(cors());
 
 // Function using RegExp to capitalise all search requests for Names and Movies
 const capitaliseTerm = (term) => {
