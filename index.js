9b5fe424fdd36081095528687937820dda73215a
simon3073 <simonbertoli@gmail.com>
2022-06-13 20:17:07 +1000

    Changed return message from remove movie from favourites

--- index.js
+++ index.js
@@ -225,7 +225,7 @@ app.put('/account/:username/movies/:movie', passport.authenticate('jwt', { sessi
 		const user = await User.findOne({ Username: req.params.username }).exec();
 		const movie = await Movies.findOne({ Title: capitalisedMovieParam }).exec();
 		await User.updateOne({ Username: user.Username }, { $addToSet: { FavouriteMovies: movie._id } }).exec();
-		res.status(200).json(FavouriteMovies);
+		res.status(200).json(User.FavouriteMovies);
 	} catch (error) {
 		console.error(error);
 		res.status(500).send(`Error: ${error}`);
